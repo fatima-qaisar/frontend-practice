eb5b424555f201d47b440f84cb2d14f8a7292e5a
Fatima Qaisar <fatima79176985@gmail.com>
2026-07-23 04:59:04 +0500

    initialize a custom react project

--- reactjs-practice/01vitereact/src/main.jsx
+++ reactjs-practice/01vitereact/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react'  //before react 18, we used to import ReactDOM from 'react-dom' and use ReactDOM.render() to render the app. Now, we import createRoot() from react-dom/client, create a React root, and then call root.render() to render the application.
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
@@ -6,5 +6,5 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
 
     <App />
-  
+  //StrictMode is a tool for highlighting potential problems in an application -> optional, but recommended for development.
 )
---
+++ reactjs-practice/customReact/custom-react.js
@@ -0,0 +1,7 @@
+// Trying to simulate how React represents a UI element internally.
+// (This is a simplified version, not the actual React object.)
+const reactElement ={
+
+}
+
+const mainContainer = document.querySelector('#root');
