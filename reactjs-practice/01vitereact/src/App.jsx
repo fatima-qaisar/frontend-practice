80b5efd9eb1e425fc82375ac05ab30f8d7e405b3
Fatima Qaisar <fatima79176985@gmail.com>
2026-07-23 04:15:00 +0500

    React components

--- reactjs-practice/01vitereact/src/App.jsx
+++ reactjs-practice/01vitereact/src/App.jsx
@@ -1,7 +1,12 @@
+import Anime from './Anime.jsx'
+
 function App() {
 
-  return (
+  return ( // A react component can return only one parent element. If we want to return multiple elements, we can wrap them in a div or a fragment. A fragment is a special type of element that does not render any HTML. It is used to group multiple elements without adding any extra nodes to the DOM.
+    <>               
     <h1>Hello React + Vite | Fatima Qaisar.</h1>
+    <Anime />
+    </>    // Fragment
   )
 }
 
---
+++ reactjs-practice/01vitereact/src/anime.jsx
@@ -0,0 +1,12 @@
+function Anime() { 
+// React components should start with an uppercase letter.
+// Lowercase tags are treated as HTML elements (e.g., <div>, <p>),
+// while uppercase names are treated as custom React components.
+    return(
+        <h3>Anime is cool!</h3>
+    ) // we can send this to any other component and use it there. This is called component reusability.
+}
+
+export default Anime
+
+//file name may be capital or small -> just keep it capital
